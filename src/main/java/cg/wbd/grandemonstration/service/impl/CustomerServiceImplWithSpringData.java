4b76a6791b11e1866d46d6402479216aa7f54320
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-07-13 14:38:54 +0700

    Initial commit

---
+++ src/main/java/cg/wbd/grandemonstration/service/impl/CustomerServiceImplWithSpringData.java
@@ -0,0 +1,114 @@
+package cg.wbd.grandemonstration.service.impl;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.repository.CustomerRepository;
+import cg.wbd.grandemonstration.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class CustomerServiceImplWithSpringData implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return streamAll().collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<Customer> findAll(Pageable pageInfo) throws Exception {
+        return customerRepository.findAll(pageInfo);
+    }
+
+    @Override
+    public List<Customer> search(String keyword) throws Exception {
+        Iterable<Customer> searchResult = customerRepository
+                .findAllByNameContainsOrEmailContainsOrAddressContains(keyword, keyword, keyword);
+        if (searchResult == null) {
+            throw new Exception("Customer not found!");
+        }
+        return streamAll(searchResult).collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<Customer> search(String keyword, Pageable pageInfo) throws Exception {
+        Page<Customer> customerPage = customerRepository.findAllByNameContainsOrEmailContainsOrAddressContains(keyword, keyword, keyword, pageInfo);
+        if (customerPage == null) {
+            throw new Exception("Customer not found!");
+        }
+        return customerPage;
+    }
+
+    @Override
+    public Customer findOne(Long id) throws Exception {
+        Customer target = customerRepository.findOne(id);
+        if (target == null) {
+            throw new Exception("Customer not found!");
+        }
+        return target;
+    }
+
+    @Override
+    public Customer save(Customer customer) throws Exception {
+        if (customer == null) {
+            throw new Exception("Wrong customer info!");
+        }
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        Iterable<Customer> updatedCustomers = customerRepository.save(customers);
+        return streamAll(updatedCustomers).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return customerRepository.exists(id);
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        Iterable<Customer> customers = customerRepository.findAll(ids);
+        return streamAll(customers).collect(Collectors.toList());
+    }
+
+    @Override
+    public long count() {
+        return customerRepository.count();
+    }
+
+    @Override
+    public void delete(Long id) {
+        customerRepository.delete(id);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        customerRepository.delete(customer);
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+        customerRepository.delete(customers);
+    }
+
+    @Override
+    public void deleteAll() {
+        customerRepository.deleteAll();
+    }
+
+    private Stream<Customer> streamAll() {
+        return StreamSupport.stream(customerRepository.findAll().spliterator(), false);
+    }
+
+    private Stream<Customer> streamAll(Iterable<Customer> customers) {
+        return StreamSupport.stream(customers.spliterator(), false);
+    }
+}
